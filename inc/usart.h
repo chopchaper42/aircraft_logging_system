5ef1170bb6b953b64e16bb15ecf205494bf2b435
chopchaper42 <vorongri@fel.cvut.cz>
2025-08-04 19:42:18 +0200

    usart implementation moved to usart.c

--- inc/usart.h
+++ inc/usart.h
@@ -29,12 +29,6 @@ typedef enum
 
 typedef enum
 {
-    STOP_BIT_1 = 0,
-    STOP_BIT_2 = 1
-} usart_stop_bit_number_t;
-
-typedef enum
-{
     BAUD_RATE_4800 = 4800,
     BAUD_RATE_9600 = 9600,
     BAUD_RATE_115_200 = 115200
@@ -45,119 +39,21 @@ struct usart
     volatile uint32_t USART_SR, USART_DR, USART_BRR, USART_CR1, USART_CR2, USART_CR3, USART_GTPR
 };
 
-void enable_usart()
-{
-    struct usart *usart = (struct usart *) USART_BASE;
-
-    // Set the UE bit to 1 in the USART_CR1 register
-    usart->USART_CR1 |= USART_CR1_UE;
-    
-}
+void enable_usart();
 
 // TODO: rewrite. Can eliminate the if-else part. Clear and set length instead
-void usart_set_word_length(usart_word_length_t length)
-{
-    struct usart *usart = (struct usart *) USART_BASE;
+void usart_set_word_length(usart_word_length_t length);
 
-    // Set the M bit to 0 in the USART_CR1 register to set word size to 8 bits
-    // Set the M to 1 for 9 bits
-    if (length == WORD_8_BITS)
-    {
-        usart->USART_CR1 &= ~USART_CR1_M;
-    }
-    else if (length == WORD_9_BITS)
-    {
-        usart->USART_CR1 |= USART_CR1_M;
-    }
-}
+void usart_set_stop_bits(usart_stop_bit_number_t stop_bits);
 
-void usart_set_stop_bits(usart_stop_bit_number_t stop_bits)
-{
-    struct usart *usart = (struct usart *) USART_BASE;
+uint32_t usart_get_oversampling_bit();
 
-    // Clears 12th and 13th bits
-    usart->USART_CR2 &= ~(0b11 << 12);
+void usart_set_baud_rate(usart_baud_rate_t baud_rate);
 
-    usart->USART_CR2 |= ((stop_bits & 0b11) << 12);
-}
+void usart_enable_transmition();
 
-uint32_t usart_get_oversampling_bit()
-{
-    struct usart *usart = (struct usart *) USART_BASE;
+void usart_init(usart_word_length_t w_length, usart_stop_bit_number_t stop_bits, usart_baud_rate_t baud_rate);
 
-    return (usart->USART_CR1 & USART_CR1_OVER8) >> 15;
-}
+void usart_send_char(char ch);
 
-void usart_set_baud_rate(usart_baud_rate_t baud_rate)
-{
-    struct usart *usart = (struct usart *) USART_BASE;
-
-    uint32_t ahb_prescaler = get_ahb_prescaler();
-    uint32_t apb2_prescaler = get_apb2_prescaler();
-    uint32_t usart_oversampling_bit = usart_get_oversampling_bit();
-
-    uint32_t usart_clock_frequency = HSI_FREQUENCY / (ahb_prescaler * apb2_prescaler);
-
-    // We scale the UART_DIV by 16 to shift it to the left by 4, allowing to avoid float arithmetics
-    uint32_t divident = 16 * usart_clock_frequency;
-    uint32_t divisor = (8 * (2 - usart_oversampling_bit) * baud_rate);
-    
-    // [(a + (b/2)) / 2] rounds the integer
-    uint32_t usart_div_scaled = (divident + (divisor / 2)) / divisor;
-
-    // Shift 4 bits to the right to get the mantissa
-    uint32_t mantissa = usart_div_scaled >> 4;
-
-    // Clear all except the last 4 bits to get the fraction
-    uint32_t fraction = usart_div_scaled & 0xF;
-
-    // When OVER8 = 1, the DIV_fraction[3] must be cleared
-    if (usart_oversampling_bit == 0b1)    
-    {
-        // Leave the first 3 less significant bits and clear the 4th
-        fraction &= 0b0111;
-    }
-
-    usart->USART_BRR = (mantissa << 4) | fraction;
-
-}
-
-void usart_enable_transmition()
-{
-    struct usart *usart = (struct usart *) USART_BASE;
-
-    // Set the TE (Transmition Enable) bit
-    usart->USART_CR1 |= USART_CR1_TE;
-}
-
-void usart_init(usart_word_length_t w_length, usart_stop_bit_number_t stop_bits, usart_baud_rate_t baud_rate)
-{
-    enable_usart();
-    usart_set_word_length(w_length);
-    usart_set_stop_bits(stop_bits);
-    usart_set_baud_rate(baud_rate);
-    usart_enable_transmition();
-}
-
-void usart_send_char(char ch)
-{
-    struct usart *usart = (struct usart *) USART_BASE;
-
-    while (!(usart->USART_SR & USART_SR_TXE));
-    usart->USART_DR = ch;
-
-    while (!(usart->USART_SR & USART_SR_TC))
-}
-
-void usart_send(const char *data, uint32_t length)
-{
-    struct usart *usart = (struct usart *) USART_BASE;
-
-    for (uint32_t i = 0; i < length || data[i] != '\0'; i++)
-    {
-        while (!(usart->USART_SR & USART_SR_TXE));
-        usart->USART_DR = data[i];
-    }
-
-    while (!(usart->USART_SR & USART_SR_TC));
-}
+void usart_send(const char *data, uint32_t length);
---
+++ src/usart.c
@@ -0,0 +1,118 @@
+#include "usart.h"
+
+void enable_usart()
+{
+    struct usart *usart = (struct usart *) USART_BASE;
+
+    // Set the UE bit to 1 in the USART_CR1 register
+    usart->USART_CR1 |= USART_CR1_UE;
+    
+}
+
+// TODO: rewrite. Can eliminate the if-else part. Clear and set length instead
+void usart_set_word_length(usart_word_length_t length)
+{
+    struct usart *usart = (struct usart *) USART_BASE;
+
+    // Set the M bit to 0 in the USART_CR1 register to set word size to 8 bits
+    // Set the M to 1 for 9 bits
+    if (length == WORD_8_BITS)
+    {
+        usart->USART_CR1 &= ~USART_CR1_M;
+    }
+    else if (length == WORD_9_BITS)
+    {
+        usart->USART_CR1 |= USART_CR1_M;
+    }
+}
+
+void usart_set_stop_bits(usart_stop_bit_number_t stop_bits)
+{
+    struct usart *usart = (struct usart *) USART_BASE;
+
+    // Clears 12th and 13th bits
+    usart->USART_CR2 &= ~(0b11 << 12);
+
+    usart->USART_CR2 |= ((stop_bits & 0b11) << 12);
+}
+
+uint32_t usart_get_oversampling_bit()
+{
+    struct usart *usart = (struct usart *) USART_BASE;
+
+    return (usart->USART_CR1 & USART_CR1_OVER8) >> 15;
+}
+
+void usart_set_baud_rate(usart_baud_rate_t baud_rate)
+{
+    struct usart *usart = (struct usart *) USART_BASE;
+
+    uint32_t ahb_prescaler = get_ahb_prescaler();
+    uint32_t apb2_prescaler = get_apb2_prescaler();
+    uint32_t usart_oversampling_bit = usart_get_oversampling_bit();
+
+    uint32_t usart_clock_frequency = HSI_FREQUENCY / (ahb_prescaler * apb2_prescaler);
+
+    // We scale the UART_DIV by 16 to shift it to the left by 4, allowing to avoid float arithmetics
+    uint32_t divident = 16 * usart_clock_frequency;
+    uint32_t divisor = (8 * (2 - usart_oversampling_bit) * baud_rate);
+    
+    // [(a + (b/2)) / 2] rounds the integer
+    uint32_t usart_div_scaled = (divident + (divisor / 2)) / divisor;
+
+    // Shift 4 bits to the right to get the mantissa
+    uint32_t mantissa = usart_div_scaled >> 4;
+
+    // Clear all except the last 4 bits to get the fraction
+    uint32_t fraction = usart_div_scaled & 0xF;
+
+    // When OVER8 = 1, the DIV_fraction[3] must be cleared
+    if (usart_oversampling_bit == 0b1)    
+    {
+        // Leave the first 3 less significant bits and clear the 4th
+        fraction &= 0b0111;
+    }
+
+    usart->USART_BRR = (mantissa << 4) | fraction;
+
+}
+
+void usart_enable_transmition()
+{
+    struct usart *usart = (struct usart *) USART_BASE;
+
+    // Set the TE (Transmition Enable) bit
+    usart->USART_CR1 |= USART_CR1_TE;
+}
+
+void usart_init(usart_word_length_t w_length, usart_stop_bit_number_t stop_bits, usart_baud_rate_t baud_rate)
+{
+    enable_usart();
+    usart_set_word_length(w_length);
+    usart_set_stop_bits(stop_bits);
+    usart_set_baud_rate(baud_rate);
+    usart_enable_transmition();
+}
+
+void usart_send_char(char ch)
+{
+    struct usart *usart = (struct usart *) USART_BASE;
+
+    while (!(usart->USART_SR & USART_SR_TXE));
+    usart->USART_DR = ch;
+
+    while (!(usart->USART_SR & USART_SR_TC));
+}
+
+void usart_send(const char *data, uint32_t length)
+{
+    struct usart *usart = (struct usart *) USART_BASE;
+
+    for (uint32_t i = 0; i < length || data[i] != '\0'; i++)
+    {
+        while (!(usart->USART_SR & USART_SR_TXE));
+        usart->USART_DR = data[i];
+    }
+
+    while (!(usart->USART_SR & USART_SR_TC));
+}
